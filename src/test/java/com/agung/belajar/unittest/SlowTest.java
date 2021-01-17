da5364d2df98619d7054b08305101dd2cca2309b
agung <agungpermadi13@gmail.com>
2021-01-17 11:05:06 +0700

    parallel unit test

--- src/test/java/com/agung/belajar/unittest/SlowTest.java
+++ src/test/java/com/agung/belajar/unittest/SlowTest.java
@@ -2,14 +2,35 @@ package com.agung.belajar.unittest;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)//jika berjalan lebih dari 5 detik maka akan dianggap gagal
-    void testSlow() throws InterruptedException {
-        Thread.sleep(10_000);//berjalan selama 10 detik
+    void testSlow1() throws InterruptedException {
+        Thread.sleep(4_000);//berjalan selama 10 detik
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)//jika berjalan lebih dari 5 detik maka akan dianggap gagal
+    void testSlow2() throws InterruptedException {
+        Thread.sleep(4_000);//berjalan selama 10 detik
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)//jika berjalan lebih dari 5 detik maka akan dianggap gagal
+    void testSlow3() throws InterruptedException {
+        Thread.sleep(4_000);//berjalan selama 10 detik
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)//jika berjalan lebih dari 5 detik maka akan dianggap gagal
+    void testSlow4() throws InterruptedException {
+        Thread.sleep(4_000);//berjalan selama 10 detik
     }
 }
