40e078905f4fe3e921a03d1d61c145a898aca092
agung <agungpermadi13@gmail.com>
2021-01-17 10:26:46 +0700

    perulangan dalam unit test

--- src/test/java/com/agung/belajar/unittest/RandomCalculatorTest.java
+++ src/test/java/com/agung/belajar/unittest/RandomCalculatorTest.java
@@ -1,7 +1,20 @@
 package com.agung.belajar.unittest;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.Random;
+
 public class RandomCalculatorTest extends AbstractCalculatorTest {
 
-
+    @Override
+    @DisplayName("Test Random Kalkulator")
+    @RepeatedTest(
+            value = 10,
+            name = "{display name} ke {currentRepetition} dari {totalRepetition}"
+    )
+    public void testRandom(Random random) {
+        super.testRandom(random);
+    }
 }
